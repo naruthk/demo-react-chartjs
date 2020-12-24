1b9cc2dddcbc7b528eb57c9a06c2632a8a0c5677
Naruth (Sam) Kongurai <naruth.kon@truedigital.com>
2020-12-24 11:28:35 +0700

    Added label to middle hole

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Doughnut } from 'react-chartjs-2';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 import { data } from "./data/data";
 
@@ -6,16 +7,36 @@ import './App.css';
 
 const options = {
   legend: {
+    display: false,
     position: "bottom"
+  },
+  rotation: 20,
+  plugins: {
+    datalabels: {
+      color: '#FFF',
+      font: {
+        size: 16,
+        weight: "bold"
+      },
+      formatter: function(value) {
+        if (value === 0) return ""; // don't show the datalabel
+        return value + " ฿";
+      }
+    }
   }
 }
 
 function App() {
   return (
     <div className="App">
-      <h1>Interactive Chart using Chart.js</h1>
-
-      <Doughnut data={data} options={options} />
+      <h1>Spending</h1>
+      <section className="spending-chart">
+        <Doughnut data={data} options={options} />
+        <div className="amount-saved-text">
+          <p className="label-save-up-to">save up to</p>
+          <p className="label-amount-saved">3500 ฿</p>
+        </div>
+      </section>
     </div>
   );
 }
